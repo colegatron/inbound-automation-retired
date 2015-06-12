19f6282267b5621e20339c02c7f472ce258b2d54
Hudson Atwell <hudson.atwell@gmail.com>
2015-06-11 19:58:48 -0500

    https://github.com/inboundnow/inbound-automation/issues/10

--- definitions/trigger.page_tracking_event.php
+++ definitions/trigger.page_tracking_event.php
@@ -11,8 +11,12 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Update_Lead' ) ) {
 
 	class Inbound_Automation_Trigger_Update_Lead {
 
+        static $trigger;
+
 		function __construct() {
+            self::$trigger = 'wplead_page_view';
 			add_filter( 'inbound_automation_triggers' , array( __CLASS__ , 'define_trigger' ) , 1 , 1);
+            add_action( 'activate/inbound-automation' , array( __CLASS__ , 'create_dummy_event' ) );
 		}
 
 		/* Build Trigger Definitions */
@@ -20,7 +24,7 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Update_Lead' ) ) {
 
 			/* Set & Extend Trigger Argument Filters */
 			$arguments = apply_filters('inbound_automation_trigger_arguments-track-lead' , array(
-					array(
+					'lead_data' => array(
 						'id' => 'lead_data',
 						'label' => 'Lead Data'
 					)
@@ -29,21 +33,21 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Update_Lead' ) ) {
 			/* Set & Extend Action DB Lookup Filters */
 			$db_lookup_filters = apply_filters( 'inbound_automation_db_lookup_filters-update-lead' , array (
 				array(
-						'id' => 'lead_data',
-						'label' => __( 'Validate Lead Data', 'inbound-pro' ),
-						'class_name' => 'Inbound_Automation_Query_Lead'
-					)
+                    'id' => 'lead_data',
+                    'label' => __( 'Validate Lead Data', 'inbound-pro' ),
+                    'class_name' => 'Inbound_Automation_Query_Lead'
+                )
 			));
 
 			/* Set & Extend Available Actions */
 			$actions = apply_filters('inbound_automation_trigger_actions-update-lead' , array(
-				'send_email' , 'wait' , 'relay_data' , 'add_lead_to_list'
+				'send_email' , 'wait' , 'relay_data' , 'add_remove_lead_list'
 			) );
 
-			$triggers['wplead_page_view'] = array (
+			$triggers[ self::$trigger ] = array (
 				'label' => 'On Lead Tracking Event',
 				'description' => 'This trigger fires whenever a tracked lead visits a new page.',
-				'action_hook' => 'wplead_page_view',
+				'action_hook' => self::$trigger ,
 				'scheduling' => false,
 				'arguments' => $arguments,
 				'db_lookup_filters' => $db_lookup_filters,
@@ -52,6 +56,28 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Update_Lead' ) ) {
 
 			return $triggers;
 		}
+
+        /**
+         * Simulate trigger - perform on plugin activation
+         */
+        public static function create_dummy_event() {
+
+
+            $lead = array (
+                'lead_id' => 97351,
+                'nature' => 'non-conversion',
+                'json' => 0,
+                'wp_lead_uid' => 'mxb8EHq5H71LtVmOTyXINufHKwA3EaUxTpH',
+                'page_id' => 97188,
+                'current_url' => 'http://inboundsoon.dev/go/flat-ui/'
+            );
+
+            $inbound_arguments = Inbound_Options_API::get_option( 'inbound_automation' , 'arguments' );
+            $inbound_arguments = ( $inbound_arguments  ) ?  $inbound_arguments : array();
+            $inbound_arguments[self::$trigger]['lead_data'] = $lead;
+
+            Inbound_Options_API::update_option( 'inbound_automation' , 'arguments' ,  $inbound_arguments );
+        }
 	}
 
 	/* Load Trigger */
