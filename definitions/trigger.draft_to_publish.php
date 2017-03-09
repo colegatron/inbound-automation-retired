b138a15e89ea8f0fa6823d68207fdda78d9bf8db
GITUSER <hudson@inboundnow.com>
2017-03-08 19:31:16 -0600

    adding draft to publish trigger.

--- definitions/trigger.draft_to_publish.php
+++ definitions/trigger.draft_to_publish.php
@@ -8,7 +8,7 @@ Contributors: Hudson Atwell
 */
 
 
-if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
+if (!class_exists('Inbound_Automation_Trigger_Draft_To_Publish')) {
 
     class Inbound_Automation_Trigger_Publish_Post {
 
@@ -16,40 +16,38 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
 
         function __construct() {
             self::$trigger = 'draft_to_publish';
-            add_filter( 'inbound_automation_triggers' , array( __CLASS__ , 'define_trigger' ) , 1 , 1);
-            add_action( 'activate/inbound-automation' , array( __CLASS__ , 'create_dummy_event' ) );
+            add_filter('inbound_automation_triggers', array(__CLASS__, 'define_trigger'), 1, 1);
+            add_action('activate/inbound-automation', array(__CLASS__, 'create_dummy_event'));
         }
 
         /* Build Trigger Definitions */
-        public static function define_trigger( $triggers ) {
+        public static function define_trigger($triggers) {
 
             /* Set & Extend Trigger Argument Filters */
-            $arguments = apply_filters('trigger/draft_to_publish/trigger_arguments/' , array(
+            $arguments = apply_filters('trigger/draft_to_publish/trigger_arguments/', array(
                 'post_object' => array(
                     'id' => 'post_object',
-                    'label' => __( 'Post Object' , 'inbound-pro'),
+                    'label' => __('Post Object', 'inbound-pro'),
                     'callback' => array(
-                        get_class() ,  'enrich_post_data'
+                        get_class(), 'enrich_post_data'
                     )
                 )
-            ) );
+            ));
 
             /* Set & Extend Action DB Lookup Filters */
             /* no db filters */
-            $db_lookup_filters = apply_filters( 'trigger/draft_to_publish/db_arguments' , array (
-
-            ));
+            $db_lookup_filters = apply_filters('trigger/draft_to_publish/db_arguments', array());
 
             /* Set & Extend Available Actions */
-            $actions = apply_filters('trigger/draft_to_publish/actions' , array(
-                'send_email' , 'wait' , 'relay_data'
-            ) );
+            $actions = apply_filters('trigger/draft_to_publish/actions', array(
+                'send_email', 'wait', 'relay_data'
+            ));
 
             /* define trigger */
-            $triggers[ self::$trigger ] = array (
-                'label' => __('On new post publish event' , 'inbound-pro'),
-                'description' => __('This trigger fires whenever a post is published.' , 'inbound-pro'),
-                'action_hook' => self::$trigger ,
+            $triggers[self::$trigger] = array(
+                'label' => __('On draft to publish event', 'inbound-pro'),
+                'description' => __('This trigger fires whenever a post is published.', 'inbound-pro'),
+                'action_hook' => self::$trigger,
                 'arguments' => $arguments,
                 'db_lookup_filters' => $db_lookup_filters,
                 'actions' => $actions
@@ -63,21 +61,24 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
          * @param $args
          * @return array
          */
-        public static function enrich_post_data( $args ) {
+        public static function enrich_post_data($args) {
 
             if (is_object($args)) {
-                $args = (array) $args;
+                $args = (array)$args;
             }
 
             if (isset($args['ID'])) {
-                $args['tag_ids'] = wp_get_post_tags( $args['ID'], array( 'fields' => 'ids' ) );
-                $args['tag_names'] = wp_get_post_tags( $args['ID'], array( 'fields' => 'names' ) );
-                $args['cat_ids'] = wp_get_object_terms($args['ID'], 'category', array( 'fields' => 'ids' ) );
-                $args['cat_names'] = wp_get_object_terms($args['ID'], 'category', array( 'fields' => 'names' ) );
+                $args['permalink'] = get_the_permalink($args['ID']);
+                $args['featured_image'] = wp_get_attachment_url(get_post_thumbnail_id($args['ID']));
+                $args['site_name'] = get_bloginfo('name');
+                $args['tag_ids'] = wp_get_post_tags($args['ID'], array('fields' => 'ids'));
+                $args['tag_names'] = wp_get_post_tags($args['ID'], array('fields' => 'names'));
+                $args['cat_ids'] = wp_get_object_terms($args['ID'], 'category', array('fields' => 'ids'));
+                $args['cat_names'] = wp_get_object_terms($args['ID'], 'category', array('fields' => 'names'));
             }
 
             if (isset($args['post_author'])) {
-                $args['post_author_name'] = get_the_author_meta( 'user_nicename',$args['post_author'] );
+                $args['post_author_name'] = get_the_author_meta('user_nicename', $args['post_author']);
             }
 
             return $args;
@@ -88,9 +89,7 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
          */
         public static function create_dummy_event() {
 
-            $event = array (
-                'new_status' => 'publish',
-                'old_status' => 'new',
+            $event = array(
                 'post_object' => array(
                     'ID' => 444,
                     'post_author' => 2,
@@ -102,7 +101,7 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
                     'post_status' => 'publish',
                     'comment_status' => 'open',
                     'ping_status' => 'open',
-                    'post_password' =>'',
+                    'post_password' => '',
                     'post_name' => 'example-post-title',
                     'to_ping' => '',
                     'pinged' => '',
@@ -116,16 +115,16 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
                     'post_mime_type' => '',
                     'comment_count' => 0,
                     'filter' => 'raw',
-                    'tag_ids' => array('0' => 216 ),
-                    'tag_names' => array('0' => 'tags' ),
-                    'cat_ids' => array('0' => 7 ),
+                    'tag_ids' => array('0' => 216),
+                    'tag_names' => array('0' => 'tags'),
+                    'cat_ids' => array('0' => 7),
                     'cat_names' => Array('0' => 'Post Category'),
                     'post_author_name' => 'hudson-atwell'
                 )
             );
 
-            $inbound_arguments = Inbound_Options_API::get_option( 'inbound_automation' , 'arguments' );
-            $inbound_arguments = ( $inbound_arguments  ) ?  $inbound_arguments : array();
+            $inbound_arguments = Inbound_Options_API::get_option('inbound_automation', 'arguments');
+            $inbound_arguments = ($inbound_arguments) ? $inbound_arguments : array();
 
             /* if data exists do not update arguments object */
             if (isset($inbound_arguments[self::$trigger])) {
@@ -134,7 +133,7 @@ if ( !class_exists( 'Inbound_Automation_Trigger_Draft_To_Publish' ) ) {
 
             $inbound_arguments[self::$trigger] = $event;
 
-            Inbound_Options_API::update_option( 'inbound_automation' , 'arguments' ,  $inbound_arguments );
+            Inbound_Options_API::update_option('inbound_automation', 'arguments', $inbound_arguments);
         }
 
     }
